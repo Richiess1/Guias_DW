a4f47e2ff51e022177946d35cb88d5dd2c3834f4
Richiess1 <20235894@esen.edu.sv>
2024-10-29 16:06:35 -0600

    mas de la GUIAAAAAAAAA, QUE SE ACABE YAAAAAAAA

---
+++ Guia6_20235894/RecursosGuia6/js/tabla.js
@@ -0,0 +1,9 @@
+const generarFila = (tipo, fila, columnas) =>{
+    let tr = `<tr>`;
+
+    for(let c = 0; c <= columnas; c++){
+        if(c==0){
+            tr += `<th scope></th>`;
+        }
+    }
+}
