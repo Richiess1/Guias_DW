695e32bffffcdceabcb3a1e0c1ca26d43a4b1472
Richiess1 <20235894@esen.edu.sv>
2024-10-29 22:17:15 -0600

    Guias

---
+++ Guia6_20235894/RecursosGuia6/js/formulario.js
@@ -0,0 +1,183 @@
+const inputNombre = document.getElementById("idTxtNombre");
+const inputApellido = document.getElementById("idTxtApellido");
+const inputFechaNacimiento = document.getElementById("idTxtFechaNacimiento");
+const inputRdMasculino = document.getElementById("idRdMasculino");
+const inputRdFemenino = document.getElementById("idRdFemenino");
+const cmbPais = document.getElementById("idCmbPais");
+const inputDireccion = document.getElementById("idTxtDireccion");
+const inputNombrePais = document.getElementById("idNombrePais");
+const buttonAgregarPaciente = document.getElementById("idBtnAgregar");
+const buttonLimpiarPaciente = document.getElementById("idBtnLimpiar");
+const buttonMostrarPaciente = document.getElementById("idBtnMostrar");
+const buttonAgregarPais = document.getElementById("idBtnAgregarPais");
+const notificacion = document.getElementById("idNotificacion");
+
+const toast = new bootstrap.Toast(notificacion);
+const mensaje = document.getElementById("idMensaje");
+
+const idModal = document.getElementById("idModal");
+
+let arrayPaciente = [];
+let editIndex = null; 
+
+const limpiarForm = () => { 
+    inputNombre.value = "";
+    inputApellido.value = "";
+    inputFechaNacimiento.value = "";
+    inputRdMasculino.checked = false;
+    inputRdFemenino.checked = false;
+    cmbPais.value = 0;
+    inputDireccion.value = "";
+    inputNombrePais.value = "";
+
+    inputNombre.focus();
+    editIndex = null; 
+};
+
+const addPaciente = function(){
+    let nombre = inputNombre.value;
+    let apellido = inputApellido.value;
+    let fechaNacimiento = inputFechaNacimiento.value;
+    let sexo =
+        inputRdMasculino.checked == true
+        ? "Hombre"
+        : inputRdFemenino.checked == true
+        ? "Mujer"
+        : "";
+    let pais = cmbPais.value;
+    let labelPais = cmbPais.options[cmbPais.selectedIndex].text;
+    let direccion = inputDireccion.value;
+
+    if(
+        nombre != "" &&
+        apellido != "" &&
+        fechaNacimiento != "" &&
+        sexo != "" &&
+        pais != "" &&
+        direccion != "" 
+    ) {
+        const nuevoPaciente = [nombre, apellido, fechaNacimiento, sexo, labelPais, direccion];
+        
+        if (editIndex === null) {
+            arrayPaciente.push(nuevoPaciente); 
+            mensaje.innerHTML = "Se ha registrado un nuevo paciente";
+        } else {
+            arrayPaciente[editIndex] = nuevoPaciente; 
+            mensaje.innerHTML = "Paciente editado correctamente";
+        }
+        
+        toast.show();
+        limpiarForm();
+        imprimirPacientes();
+
+    } else{
+        mensaje.innerHTML = "Faltan campos por completar";
+        toast.show();
+    }
+};
+
+function eliminarPaciente(index) {
+    arrayPaciente.splice(index, 1);
+    mensaje.innerHTML = "Paciente eliminado correctamente";
+    toast.show();
+    imprimirPacientes();
+}
+
+function editarPaciente(index) {
+    const paciente = arrayPaciente[index];
+    inputNombre.value = paciente[0];
+    inputApellido.value = paciente[1];
+    inputFechaNacimiento.value = paciente[2];
+    inputRdMasculino.checked = paciente[3] === "Hombre";
+    inputRdFemenino.checked = paciente[3] === "Mujer";
+    cmbPais.value = Array.from(cmbPais.options).find(option => option.text === paciente[4]).value;
+    inputDireccion.value = paciente[5];
+
+    editIndex = index; // Establece el índice del paciente a editar
+    inputNombre.focus();
+}
+
+function imprimirFilas() {
+    let $fila = "";
+    arrayPaciente.forEach((element, index) => {
+        $fila += `<tr>
+                    <td scope="row" class="text-center fw-bold">${index + 1}</td>
+                    <td>${element[0]}</td>
+                    <td>${element[1]}</td>
+                    <td>${element[2]}</td>
+                    <td>${element[3]}</td>
+                    <td>${element[4]}</td>
+                    <td>${element[5]}</td>
+                    <td>
+                        <button onclick="editarPaciente(${index})" type="button" class="btn btn-primary" alt="Editar">
+                            <i class="bi bi-pencil-square"></i>
+                        </button>
+                        <button onclick="eliminarPaciente(${index})" type="button" class="btn btn-danger" alt="Eliminar">
+                            <i class="bi bi-trash3-fill"></i>
+                        </button>
+                    </td>
+                </tr>`;
+    });
+    return $fila;
+}
+
+const imprimirPacientes = () => {
+    let $table = `<div class="table-responsive">
+                    <table class="table table-striped table-hover table-bordered">
+                        <tr>
+                            <th scope="col" class="text-center" style="width:5%"></th>
+                            <th scope="col" class="text-center" style="width: 15%">Nombre</th>
+                            <th scope="col" class="text-center" style="width:15%">Apellido</th>
+                            <th scope="col" class="text-center" style="width:10%">Fecha nacimiento</th>
+                            <th scope="col" class="text-center" style="width:10%">Sexo</th>
+                            <th scope="col" class="text-center" style="width: 10%">País</th>
+                            <th scope="col" class="text-center" style="width:25%">Dirección</th>
+                            <th scope="col" class="text-center" style="width:10%">Opciones</th>
+                        </tr>
+                        ${imprimirFilas()}
+                    </table>
+                </div>`;
+    document.getElementById("idTablaPacientes").innerHTML = $table;
+}
+
+let contadorGlobalOption = cmbPais.children.length;
+const addPais = () => {
+    let paisNew = inputNombrePais.value;
+
+    if (paisNew != "") {
+        let option = document.createElement("option");
+        option.textContent = paisNew;
+        option.value = contadorGlobalOption + 1;
+        cmbPais.appendChild(option);
+
+        mensaje.innerHTML = "País agregado correctamente";
+        toast.show();
+
+    } else {
+        mensaje.innerHTML = "Faltan campos por completar";
+        toast.show();
+    }
+};
+
+buttonLimpiarPaciente.onclick = () => {
+    limpiarForm();
+};
+
+buttonAgregarPaciente.onclick = () => {
+    addPaciente();
+};
+
+buttonMostrarPaciente.onclick = () => {
+    imprimirPacientes();
+};
+
+buttonAgregarPais.onclick = () => {
+    addPais();
+};
+
+idModal.addEventListener("show.bs.modal", () => {
+    inputNombrePais.value = "";
+    inputNombrePais.focus();
+});
+
+limpiarForm();
